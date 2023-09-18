6a5cfdaa5efa0761ae1a0726e32d6d25b4850d4a
Grabbing <bllebblleb14@gmail.com>
2023-09-18 20:52:19 +0700

    Update database.go
    Clear the hard code and use .env to import environment variables.

--- config/database.go
+++ config/database.go
@@ -2,16 +2,36 @@ package config
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
+
 var DB *gorm.DB
 
 func InitDB() {
+	LoadEnv() // Load environment variables
+
+	// Retrieve environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Construct the DSN using environment variables
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
 	var err error
-	dsn := "root:example@tcp(localhost:3306)/godb?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
