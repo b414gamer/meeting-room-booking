48f69b8b22b78e665c9f6d9a0df91ba68fb02601
Grabbing <bllebblleb14@gmail.com>
2023-09-20 16:45:05 +0700

    Update and add column for Booking Table
    -Delete Date
    -Add UserID
    -Add StartTime
    -Add EndTime

--- models/booking.go
+++ models/booking.go
@@ -4,8 +4,9 @@ import "time"
 
 type Booking struct {
 	BookingID uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null"`
 	RoomID    uint      `gorm:"not null"`
-	Date      time.Time `gorm:"not null"`
+	UserID    uint      `gorm:"not null"`
+	StartTime time.Time `gorm:"not null"`
+	EndTime   time.Time `gorm:"not null"`
 	CreatedAt time.Time
 }
