84dd6f197ce72f75fb753cfd7151cfd46b805d2b
Grabbing <bllebblleb14@gmail.com>
2023-09-20 14:44:50 +0700

    Update Room handlers
    1.The RoomWithStatus struct combines the room details with its status, which will be useful for the frontend to display the room information along with its current status.
    2.The GetRoomStatus function checks for active bookings for a given room and returns the status as "Booked" or "Available".
    3.The ListRoomsHandler function fetches all rooms, determines their status, and returns them in the response.

--- handlers/room_handlers.go
+++ handlers/room_handlers.go
@@ -5,7 +5,26 @@ import (
 	"meeting-room-booking/config"
 	"meeting-room-booking/models"
 	"net/http"
+	"time"
 )
+
+type RoomWithStatus struct {
+	models.Room
+	Status string `json:"status"`
+}
+
+func GetRoomStatus(roomID uint) string {
+	var bookings []models.Booking
+	currentTime := time.Now()
+
+	//Fetch bookings for the room that are active (i.e.  current time is between start and end time)
+	config.DB.Where("room_id = ? AND start_time <= ? AND end_time >= ?", roomID, currentTime, currentTime).Find(&bookings)
+
+	if len(bookings) > 0 {
+		return "Booked"
+	}
+	return "Available"
+}
 
 func ListRoomsHandler(w http.ResponseWriter, r *http.Request) {
 	var rooms []models.Room
@@ -17,7 +36,20 @@ func ListRoomsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//Return the list of rooms in the response
+	//Create a slice to hold rooms with their statuses
+	var roomWithStatus []RoomWithStatus
+
+	//Determine the status of each room
+	for _, room := range rooms {
+		status := GetRoomStatus(room.RoomID)
+		roomWithStatus = append(roomWithStatus, RoomWithStatus{room, status})
+	}
+
+	//Return the list of rooms with their statuses in the response
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(rooms)
+	json.NewEncoder(w).Encode(roomWithStatus)
+
+	// //Return the list of rooms in the response
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(rooms)
 }
